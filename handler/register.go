8d802190da67a4edd07036b803b8c41da3cbdf80
Alex Korzh <alexkorzh7@gmail.com>
2020-04-13 18:23:40 +0200

    Register file handlers

--- handler/register.go
+++ handler/register.go
@@ -5,4 +5,6 @@ import "github.com/AlexKLWS/lws-blog-server/router"
 func RegisterHandlers(serverRouter *router.Router) {
 	serverRouter.Auth.POST("/login", Login)
 	serverRouter.Articles.PUT("", NewArticle)
+	serverRouter.Files.PUT("/metadata", NewFileMetaData)
+	serverRouter.Files.PUT("", NewFiles)
 }
