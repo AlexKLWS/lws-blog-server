439a3dbbc63d88b34c17df5dc7668ad28c2fce51
Alex Korzh <alexkorzh7@gmail.com>
2020-04-23 23:54:55 +0200

    Extend model definitions so that gorm can resolve it properly

--- models/article.go
+++ models/article.go
@@ -1,8 +1,12 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type ArticleData struct {
+	gorm.Model
 	Name        string   `json:"name" xml:"name"`
 	Subtitle    string   `json:"subtitle" xml:"subtitle"`
 	ArticleText string   `json:"articleText" xml:"articleText"`
-	Icon        IconData `json:"icon" xml:"icon"`
+	Icon        IconData `json:"icon" xml:"icon" gorm:"foreignkey:IconRefer"`
+	IconRefer uint
 }
--- models/icon.go
+++ models/icon.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type IconData struct {
+	gorm.Model
 	Data   string `json:"data" xml:"data"`
 	Height string `json:"height" xml:"height"`
 	Width  string `json:"width" xml:"width"`
