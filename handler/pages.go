cd24e8d6627e1934a6caafa8839447a96f1b4f73
Alex Korzh <alexkorzh7@gmail.com>
2020-04-26 20:21:59 +0200

    Add new page handler

---
+++ handler/pages.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/AlexKLWS/lws-blog-server/models"
+	"github.com/AlexKLWS/lws-blog-server/pages"
+	"github.com/labstack/echo"
+	"log"
+	"net/http"
+)
+
+func NewPage(c echo.Context) error {
+	pageData := models.PageData{}
+
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&pageData)
+	if err != nil {
+		log.Printf("Failed processing page submit request: %s\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+
+	go pages.Create(&pageData)
+
+	return c.String(http.StatusOK, "OK")
+}
--- handler/register.go
+++ handler/register.go
@@ -5,6 +5,7 @@ import "github.com/AlexKLWS/lws-blog-server/router"
 func RegisterHandlers(serverRouter *router.Router) {
 	serverRouter.Auth.POST("/login", Login)
 	serverRouter.Articles.PUT("", NewArticle)
+	serverRouter.Pages.PUT("", NewPage)
 	serverRouter.Files.PUT("/metadata", AddNewFileMetaData)
 	serverRouter.Files.PUT("", AddNewFiles)
 }
