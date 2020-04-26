250c2a1832df462101ae250fdca15c0e7d36d97b
Alex Korzh <alexkorzh7@gmail.com>
2020-04-26 20:13:16 +0200

    Add functions to write articles and pages to db

---
+++ articles/create.go
@@ -0,0 +1,26 @@
+package articles
+
+import (
+	"github.com/AlexKLWS/lws-blog-server/config"
+	"github.com/AlexKLWS/lws-blog-server/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/spf13/viper"
+	"log"
+)
+
+func Create(article *models.ArticleData) {
+	db, err := gorm.Open(viper.GetString(config.GormDialect), viper.GetString(config.GormConnectionString))
+	if err != nil {
+		log.Printf("DB open error: %s\n", err)
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&models.IconData{})
+	db.AutoMigrate(&models.ArticleData{})
+
+	// Create
+	db.Create(article)
+}
