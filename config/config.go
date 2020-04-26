7fc0b0e61c421459507b5c0419447c50fff14b64
Alex Korzh <alexkorzh7@gmail.com>
2020-04-26 19:57:34 +0200

    Watch config change

--- config/config.go
+++ config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	"log"
 )
 
 func InitializeViper() {
@@ -14,4 +16,9 @@ func InitializeViper() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
+	viper.WatchConfig()
+
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Print("Viper config change registered!")
+	})
 }
