557f737835801502add7e7f37db17c453d87d76f
Alex Korzh <alexkorzh7@gmail.com>
2020-05-02 21:49:56 +0200

    Implement material record interface

--- models/article.go
+++ models/article.go
@@ -1,9 +1,20 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
 type ArticleData struct {
 	gorm.Model
 	MaterialData
 	ArticleText string `json:"articleText" xml:"articleText"`
+}
+
+func (ad ArticleData) getID() uint {
+	return ad.ID
+}
+
+func (ad ArticleData) getCreatedAt() time.Time {
+	return ad.CreatedAt
 }
--- models/page.go
+++ models/page.go
@@ -1,9 +1,20 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
 type PageData struct {
 	gorm.Model
 	MaterialData
 	PageURL string `json:"pageURL" xml:"pageURL"`
+}
+
+func (pg PageData) getID() uint {
+	return pg.ID
+}
+
+func (pg PageData) getCreatedAt() time.Time {
+	return pg.CreatedAt
 }
