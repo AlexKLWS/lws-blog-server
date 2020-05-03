8d6c8ca0aa081e2316866462e6e528ac898dd17a
Alex Korzh <alexkorzh7@gmail.com>
2020-05-03 15:51:14 +0200

    Select only 20 most recent records

--- handler/materials.go
+++ handler/materials.go
@@ -1,10 +1,23 @@
 package handler
 
 import (
+	"github.com/AlexKLWS/lws-blog-server/materials"
+	"github.com/AlexKLWS/lws-blog-server/models"
 	"github.com/labstack/echo"
 	"net/http"
+	"sort"
 )
 
 func GetMaterials(c echo.Context) error {
-	return c.String(http.StatusOK, "")
+	allMaterialRecords := materials.Get()
+	sort.Slice(allMaterialRecords, func(i, j int) bool {
+		return allMaterialRecords[i].GetCreatedAt().After(allMaterialRecords[j].GetCreatedAt())
+	})
+	var selectedMaterialRecords []models.MaterialRecord
+	if len(allMaterialRecords) > 20 {
+		selectedMaterialRecords = allMaterialRecords[:20]
+	} else {
+		selectedMaterialRecords = allMaterialRecords
+	}
+ 	return c.JSON(http.StatusOK, selectedMaterialRecords)
 }
