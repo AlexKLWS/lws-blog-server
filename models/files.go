8aecfad277c19b425af6f352a162bfa480a83b6a
Alex Korzh <alexkorzh7@gmail.com>
2020-05-10 15:41:46 +0200

    Add reference id to file metadata and material

--- models/files.go
+++ models/files.go
@@ -5,7 +5,8 @@ type UploadMetaDataBody struct {
 }
 
 type FileMetaData struct {
-	Id      string `json:"id" xml:"id"`
-	NewName string `json:"newName" xml:"newName"`
-	Folder  string `json:"folder" xml:"folder"`
+	Id          string `json:"id" xml:"id"`
+	ReferenceId string `json:"referenceId" xml:"referenceId"`
+	NewName     string `json:"newName" xml:"newName"`
+	Folder      string `json:"folder" xml:"folder"`
 }
--- models/material.go
+++ models/material.go
@@ -1,9 +1,10 @@
 package models
 
 type MaterialData struct {
-	Name      string   `json:"name" xml:"name"`
-	Subtitle  string   `json:"subtitle" xml:"subtitle"`
-	Category  Category `json:"category" xml:"category"`
-	Icon      IconData `json:"icon" xml:"icon" gorm:"foreignkey:IconRefer"`
-	IconRefer uint     `json:"-"`
+	Name        string   `json:"name" xml:"name"`
+	Subtitle    string   `json:"subtitle" xml:"subtitle"`
+	Category    Category `json:"category" xml:"category"`
+	Icon        IconData `json:"icon" xml:"icon" gorm:"foreignkey:IconRefer"`
+	ReferenceId string   `json:"referenceId" xml:"referenceId" gorm:"unique;not null"`
+	IconRefer   uint     `json:"-"`
 }
