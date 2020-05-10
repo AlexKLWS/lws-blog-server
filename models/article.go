b63f73acda5ecda0574e298f4abbaa95087ae11c
Alex Korzh <alexkorzh7@gmail.com>
2020-05-10 15:40:36 +0200

    Add helper creating model from joined record

--- models/article.go
+++ models/article.go
@@ -17,3 +17,19 @@ func (ad ArticleData) GetID() uint {
 func (ad ArticleData) GetCreatedAt() time.Time {
 	return ad.CreatedAt
 }
+
+func CreateArticleDataFromJoinedRecord(r JoinedArticlePage) ArticleData {
+	return ArticleData{
+		Model: Model{
+			CreatedAt: r.CreatedAt,
+		},
+		MaterialData: MaterialData{
+			Name:        r.Name,
+			Subtitle:    r.Subtitle,
+			Category:    r.Category,
+			Icon:        IconData{},
+			ReferenceId: r.ReferenceId,
+			IconRefer:   0,
+		},
+	}
+}
--- models/page.go
+++ models/page.go
@@ -17,3 +17,20 @@ func (pg PageData) GetID() uint {
 func (pg PageData) GetCreatedAt() time.Time {
 	return pg.CreatedAt
 }
+
+func CreatePageDataFromJoinedRecord(r JoinedArticlePage) PageData {
+	return PageData{
+		Model: Model{
+			CreatedAt: r.CreatedAt,
+		},
+		MaterialData: MaterialData{
+			Name:        r.Name,
+			Subtitle:    r.Subtitle,
+			Category:    r.Category,
+			Icon:        IconData{},
+			ReferenceId: r.ReferenceId,
+			IconRefer:   0,
+		},
+		PageURL: r.PageURL,
+	}
+}
