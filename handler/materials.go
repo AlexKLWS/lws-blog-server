593ff593fbbd46b5069666d491e2e92ba10e8da6
Alex Korzh <alexkorzh7@gmail.com>
2020-05-10 18:51:11 +0200

    Make handler materials fetch from certain date

--- handler/materials.go
+++ handler/materials.go
@@ -1,15 +1,38 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/AlexKLWS/lws-blog-server/materials"
 	"github.com/AlexKLWS/lws-blog-server/models"
 	"github.com/labstack/echo"
+	"log"
 	"net/http"
 	"sort"
+	"time"
 )
 
 func GetMaterials(c echo.Context) error {
-	allMaterialRecords := materials.Get()
+	data := struct{ FromDate string }{}
+
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&data)
+	var allMaterialRecords []models.MaterialRecord
+	if err != nil {
+		if err.Error() == "EOF" {
+			allMaterialRecords = materials.Get()
+		} else {
+			log.Printf("Failed processing article request: %s\n", err)
+			return echo.NewHTTPError(http.StatusInternalServerError)
+		}
+	} else {
+		t, tError := time.Parse(time.RFC3339, data.FromDate)
+		if tError != nil {
+			log.Printf("Failed parsing from date: %s\n", err)
+			return echo.NewHTTPError(http.StatusInternalServerError)
+		}
+		allMaterialRecords = materials.GetFromDate(t)
+	}
+
 	sort.Slice(allMaterialRecords, func(i, j int) bool {
 		return allMaterialRecords[i].GetCreatedAt().After(allMaterialRecords[j].GetCreatedAt())
 	})
@@ -19,5 +42,5 @@ func GetMaterials(c echo.Context) error {
 	} else {
 		selectedMaterialRecords = allMaterialRecords
 	}
- 	return c.JSON(http.StatusOK, selectedMaterialRecords)
+	return c.JSON(http.StatusOK, selectedMaterialRecords)
 }
