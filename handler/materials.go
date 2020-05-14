9b6965cdf06502115ae69b170c785d3a5c894b10
Alex Korzh <alexkorzh7@gmail.com>
2020-05-14 23:05:27 +0200

    Get materials for category

--- handler/materials.go
+++ handler/materials.go
@@ -2,21 +2,26 @@ package handler
 
 import (
 	"encoding/json"
-	"github.com/AlexKLWS/lws-blog-server/materials"
-	"github.com/AlexKLWS/lws-blog-server/models"
-	"github.com/labstack/echo"
 	"log"
 	"net/http"
 	"sort"
 	"time"
+
+	"github.com/AlexKLWS/lws-blog-server/materials"
+	"github.com/AlexKLWS/lws-blog-server/models"
+	"github.com/labstack/echo"
 )
 
 func GetMaterials(c echo.Context) error {
-	data := struct{ FromDate string }{}
+	data := struct {
+		FromDate string
+		Category models.Category
+	}{}
 
 	defer c.Request().Body.Close()
 	err := json.NewDecoder(c.Request().Body).Decode(&data)
 	var allMaterialRecords []models.MaterialRecord
+	// Maybe refactor this bit later
 	if err != nil {
 		if err.Error() == "EOF" {
 			allMaterialRecords = materials.Get()
@@ -25,12 +30,24 @@ func GetMaterials(c echo.Context) error {
 			return echo.NewHTTPError(http.StatusInternalServerError)
 		}
 	} else {
-		t, tError := time.Parse(time.RFC3339, data.FromDate)
-		if tError != nil {
-			log.Printf("Failed parsing from date: %s\n", err)
-			return echo.NewHTTPError(http.StatusInternalServerError)
+		if data.FromDate != "" {
+			t, tError := time.Parse(time.RFC3339, data.FromDate)
+			if tError != nil {
+				log.Printf("Failed parsing from date: %s\n", err)
+				return echo.NewHTTPError(http.StatusInternalServerError)
+			}
+			if data.Category != 0 {
+				allMaterialRecords = materials.GetFromDateForCategory(t, data.Category)
+			} else {
+				allMaterialRecords = materials.GetFromDate(t)
+			}
+		} else {
+			if data.Category != 0 {
+				allMaterialRecords = materials.GetForCategory(data.Category)
+			} else {
+				allMaterialRecords = materials.Get()
+			}
 		}
-		allMaterialRecords = materials.GetFromDate(t)
 	}
 
 	sort.Slice(allMaterialRecords, func(i, j int) bool {
--- materials/get.go
+++ materials/get.go
@@ -2,12 +2,13 @@ package materials
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/AlexKLWS/lws-blog-server/config"
 	"github.com/AlexKLWS/lws-blog-server/models"
 	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
-	"log"
-	"time"
 )
 
 func GetFromDate(date time.Time) []models.MaterialRecord {
@@ -43,6 +44,39 @@ func GetFromDate(date time.Time) []models.MaterialRecord {
 	return materials
 }
 
+func GetFromDateForCategory(date time.Time, category models.Category) []models.MaterialRecord {
+	db, err := gorm.Open(viper.GetString(config.GormDialect), viper.GetString(config.GormConnectionString))
+	if err != nil {
+		log.Printf("DB open error: %s\n", err)
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	var materials []models.MaterialRecord
+	var intermediateData []models.JoinedArticlePage
+	db.Table(config.PagesTableName).
+		Where("created_at > ? AND category = ?", date, category).
+		Select("*").
+		Joins(fmt.Sprintf("JOIN %s ON %s.icon_refer = %s.id", config.IconTableName, config.PagesTableName, config.IconTableName)).
+		Find(&intermediateData)
+	for i := range intermediateData {
+		page := models.CreatePageDataFromJoinedRecord(intermediateData[i])
+		materials = append(materials, page)
+	}
+
+	db.Table(config.ArticleTableName).
+		Where("created_at > ? AND category = ?", date, category).
+		Select("*").
+		Joins(fmt.Sprintf("JOIN %s ON %s.icon_refer = %s.id", config.IconTableName, config.ArticleTableName, config.IconTableName)).
+		Find(&intermediateData)
+	for i := range intermediateData {
+		article := models.CreateArticleDataFromJoinedRecord(intermediateData[i])
+		materials = append(materials, article)
+	}
+
+	return materials
+}
+
 func Get() []models.MaterialRecord {
 	db, err := gorm.Open(viper.GetString(config.GormDialect), viper.GetString(config.GormConnectionString))
 	if err != nil {
@@ -63,6 +97,39 @@ func Get() []models.MaterialRecord {
 	}
 
 	db.Table(config.ArticleTableName).
+		Select("*").
+		Joins(fmt.Sprintf("JOIN %s ON %s.icon_refer = %s.id", config.IconTableName, config.ArticleTableName, config.IconTableName)).
+		Find(&intermediateData)
+	for i := range intermediateData {
+		article := models.CreateArticleDataFromJoinedRecord(intermediateData[i])
+		materials = append(materials, article)
+	}
+
+	return materials
+}
+
+func GetForCategory(category models.Category) []models.MaterialRecord {
+	db, err := gorm.Open(viper.GetString(config.GormDialect), viper.GetString(config.GormConnectionString))
+	if err != nil {
+		log.Printf("DB open error: %s\n", err)
+		panic("Failed to connect database")
+	}
+	defer db.Close()
+
+	var materials []models.MaterialRecord
+	var intermediateData []models.JoinedArticlePage
+	db.Table(config.PagesTableName).
+		Where("category = ?", category).
+		Select("*").
+		Joins(fmt.Sprintf("JOIN %s ON %s.icon_refer = %s.id", config.IconTableName, config.PagesTableName, config.IconTableName)).
+		Find(&intermediateData)
+	for i := range intermediateData {
+		page := models.CreatePageDataFromJoinedRecord(intermediateData[i])
+		materials = append(materials, page)
+	}
+
+	db.Table(config.ArticleTableName).
+		Where("category = ?", category).
 		Select("*").
 		Joins(fmt.Sprintf("JOIN %s ON %s.icon_refer = %s.id", config.IconTableName, config.ArticleTableName, config.IconTableName)).
 		Find(&intermediateData)
