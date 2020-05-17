03ab601923c4f25060a5acd6ab7d1b4889b1a769
Alex Korzh <alexkorzh7@gmail.com>
2020-05-17 23:07:16 +0200

    Make get materials handler expect query params, change page size

--- handler/materials.go
+++ handler/materials.go
@@ -1,10 +1,10 @@
 package handler
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/AlexKLWS/lws-blog-server/materials"
@@ -13,40 +13,32 @@ import (
 )
 
 func GetMaterials(c echo.Context) error {
-	data := struct {
-		FromDate string
-		Category models.Category
-	}{}
+	categoryNumber, conversionError := strconv.Atoi(c.QueryParam("category"))
+	if conversionError != nil {
+		log.Printf("Failed parsing category: %s\n", conversionError)
+		return echo.NewHTTPError(http.StatusInternalServerError)
+	}
+	category := models.Category(categoryNumber)
+	fromDate := c.QueryParam("fromDate")
 
-	defer c.Request().Body.Close()
-	err := json.NewDecoder(c.Request().Body).Decode(&data)
 	var allMaterialRecords []models.MaterialRecord
 	// Maybe refactor this bit later
-	if err != nil {
-		if err.Error() == "EOF" {
-			allMaterialRecords = materials.Get()
-		} else {
-			log.Printf("Failed processing article request: %s\n", err)
+	if fromDate != "" {
+		t, tError := time.Parse(time.RFC3339, fromDate)
+		if tError != nil {
+			log.Printf("Failed parsing from date: %s\n", tError)
 			return echo.NewHTTPError(http.StatusInternalServerError)
 		}
-	} else {
-		if data.FromDate != "" {
-			t, tError := time.Parse(time.RFC3339, data.FromDate)
-			if tError != nil {
-				log.Printf("Failed parsing from date: %s\n", err)
-				return echo.NewHTTPError(http.StatusInternalServerError)
-			}
-			if data.Category != 0 {
-				allMaterialRecords = materials.GetFromDateForCategory(t, data.Category)
-			} else {
-				allMaterialRecords = materials.GetFromDate(t)
-			}
+		if category != 0 {
+			allMaterialRecords = materials.GetFromDateForCategory(t, category)
 		} else {
-			if data.Category != 0 {
-				allMaterialRecords = materials.GetForCategory(data.Category)
-			} else {
-				allMaterialRecords = materials.Get()
-			}
+			allMaterialRecords = materials.GetFromDate(t)
+		}
+	} else {
+		if category != 0 {
+			allMaterialRecords = materials.GetForCategory(category)
+		} else {
+			allMaterialRecords = materials.Get()
 		}
 	}
 
@@ -54,8 +46,8 @@ func GetMaterials(c echo.Context) error {
 		return allMaterialRecords[i].GetCreatedAt().After(allMaterialRecords[j].GetCreatedAt())
 	})
 	var selectedMaterialRecords []models.MaterialRecord
-	if len(allMaterialRecords) > 20 {
-		selectedMaterialRecords = allMaterialRecords[:20]
+	if len(allMaterialRecords) > 16 {
+		selectedMaterialRecords = allMaterialRecords[:16]
 	} else {
 		selectedMaterialRecords = allMaterialRecords
 	}
