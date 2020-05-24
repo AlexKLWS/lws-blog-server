131da05cc9685ea22ea272755310395c618ceff2
Alex Korzh <alexkorzh7@gmail.com>
2020-05-24 20:56:45 +0200

    Simply quit instead of panicing when creating article db error

--- articles/create.go
+++ articles/create.go
@@ -1,19 +1,20 @@
 package articles
 
 import (
+	"log"
+
 	"github.com/AlexKLWS/lws-blog-server/config"
 	"github.com/AlexKLWS/lws-blog-server/models"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/spf13/viper"
-	"log"
 )
 
 func Create(article *models.ArticleData) {
 	db, err := gorm.Open(viper.GetString(config.GormDialect), viper.GetString(config.GormConnectionString))
 	if err != nil {
 		log.Printf("DB open error: %s\n", err)
-		panic("Failed to connect database")
+		return
 	}
 	defer db.Close()
 
