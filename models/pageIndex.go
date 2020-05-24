9984157f815f3399574a782bd8170900f88fc429
Alex Korzh <alexkorzh7@gmail.com>
2020-05-24 20:56:57 +0200

    Tweak page index struct

--- models/pageIndex.go
+++ models/pageIndex.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 type PageIndex struct {
 	Model
-	Page  int    `json:"page" xml:"page"`
-	Query string `json:"query" xml:"query"`
+	Page      int       `json:"page" xml:"page"`
+	StartDate time.Time `json:"startDate" xml:"startDate"`
+	EndDate   time.Time `json:"endDate" xml:"endDate"`
 }
