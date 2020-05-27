c57e5f2d77a1954c064b576dbc904bcc4926ee6d
Alex Korzh <alexkorzh7@gmail.com>
2020-05-27 20:25:25 +0200

    Add category to page index

--- models/pageIndex.go
+++ models/pageIndex.go
@@ -7,4 +7,5 @@ type PageIndex struct {
 	Page      int       `json:"page" xml:"page"`
 	StartDate time.Time `json:"startDate" xml:"startDate"`
 	EndDate   time.Time `json:"endDate" xml:"endDate"`
+	Category  Category  `json:"category" xml:"category"`
 }
